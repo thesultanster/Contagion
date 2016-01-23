fa681d0aaa9a5113936751df5c0602f7a6a38621
Sultan Khan <khansultan024@gmail.com>
2016-01-22 22:15:07 -0800

    Users can join game and quit game. Players array is updated.

--- app/src/main/java/tag/zombie/contation/com/contagion/GameActivity.java
+++ app/src/main/java/tag/zombie/contation/com/contagion/GameActivity.java
@@ -1,12 +1,24 @@
 package tag.zombie.contation.com.contagion;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -18,6 +30,10 @@ public class GameActivity extends AppCompatActivity {
     RelativeLayout userStateLayout;
     View userStateView;
     TextView userStateTextView;
+
+    Button itButton;
+    Button quitButton;
+
     double myX, myY;
 
     @Override
@@ -26,12 +42,15 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         // Inflate Variables
-        userStateView = findViewById(R.id.userstate);
-        userStateLayout = (RelativeLayout) userStateView;
-        userStateView = findViewById(R.id.user_state);
-        userStateTextView = (TextView) userStateView;
-        heartView = findViewById(R.id.heart);
-        heartImage = (ImageView) heartView;
+        inflateVariables();
+
+        // Set On Click Listeners
+        SetClickListeners();
+
+        //Start Listener Thread
+        StartListenerThread();
+
+
 
     }
 
@@ -46,7 +65,20 @@ public class GameActivity extends AppCompatActivity {
         StartHeartAnimation();
     }
 
-    private void StartHeartAnimation(){
+    private void inflateVariables() {
+        // Inflate Variables
+        userStateView = findViewById(R.id.userstate);
+        userStateLayout = (RelativeLayout) userStateView;
+        userStateView = findViewById(R.id.user_state);
+        userStateTextView = (TextView) userStateView;
+        heartView = findViewById(R.id.heart);
+        heartImage = (ImageView) heartView;
+        itButton = (Button) findViewById(R.id.itButton);
+        quitButton = (Button) findViewById(R.id.quitButton);
+
+    }
+
+    private void StartHeartAnimation() {
 
         // set its background to our AnimationDrawable XML resource.
         heartImage.setBackgroundResource(R.drawable.heart_animation_healthy);
@@ -61,4 +93,84 @@ public class GameActivity extends AppCompatActivity {
         // Start the animation (looped playback by default).
         frameAnimation.start();
     }
+
+    private void UpdateGame(){
+
+    }
+
+    private void StartListenerThread(){
+        Thread listenerThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                UpdateGame();
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+
+        listenerThread.start();
+    }
+
+
+    private void SetClickListeners(){
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ParseQuery<ParseObject> query = new ParseQuery("Game");
+                query.whereEqualTo("objectId", "m3rnAai0Hf");
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+
+
+
+                        // remove user to array of players
+                        //TODO: Convert this to a parse Cloud Code Function
+                        //==============================================================================================
+                        ArrayList<String> testStringArrayList = (ArrayList<String>) objects.get(0).get("players");
+
+                        if (testStringArrayList == null)
+                            testStringArrayList = new ArrayList<String>();
+
+
+                        testStringArrayList.remove(ParseUser.getCurrentUser().getObjectId());
+                        objects.get(0).put("players", testStringArrayList);
+                        objects.get(0).saveInBackground();
+
+                        // TODO: Find out if user is healthy or infected and decrement that
+                        // Decrement healthy player count
+                        objects.get(0).increment("healthyCount", -1);
+                        objects.get(0).saveInBackground();
+                        //==============================================================================================
+
+
+
+
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(intent);
+                        Log.d("MyApp", "Anonymous user logged in.");
+                    }
+
+
+                });
+
+
+
+
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/tag/zombie/contation/com/contagion/LoginActivity.java
+++ app/src/main/java/tag/zombie/contation/com/contagion/LoginActivity.java
@@ -3,9 +3,21 @@ package tag.zombie.contation.com.contagion;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.FindCallback;
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,8 +37,57 @@ public class LoginActivity extends AppCompatActivity {
 
     View.OnClickListener buttonListener = new View.OnClickListener() {
         public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(),GameActivity.class);
-            startActivity(intent);
+
+            ParseAnonymousUtils.logIn(new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (e != null) {
+                        Log.d("MyApp", "Anonymous login failed.");
+                    } else {
+
+                        ParseQuery<ParseObject> query = new ParseQuery("Game");
+                        query.whereEqualTo("objectId", "m3rnAai0Hf");
+                        query.findInBackground(new FindCallback<ParseObject>() {
+                            @Override
+                            public void done(List<ParseObject> objects, ParseException e) {
+
+
+
+                                // Add user to array of players
+                                //TODO: Convert this to a parse Cloud Code Function
+                                //==============================================================================================
+                                ArrayList<String> testStringArrayList = (ArrayList<String>) objects.get(0).get("players");
+
+                                if (testStringArrayList == null)
+                                    testStringArrayList = new ArrayList<String>();
+
+                                testStringArrayList.add(ParseUser.getCurrentUser().getObjectId());
+                                objects.get(0).put("players", testStringArrayList);
+                                objects.get(0).saveInBackground();
+
+                                // Increment healthy player count
+                                objects.get(0).increment("healthyCount");
+                                objects.get(0).saveInBackground();
+                                //==============================================================================================
+
+
+
+                                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                                startActivity(intent);
+                                Log.d("MyApp", "Anonymous user logged in.");
+                            }
+
+
+                        });
+
+
+
+                    }
+                }
+            });
+
+
+
         }
     };
 

067f51e6238eedb7f34bd97b425918db83e675ba
Amanda Berryhill <mylife2ski@gmail.com>
2016-01-22 20:55:29 -0800

    Added intent from login screen to the game screen

--- app/src/main/java/tag/zombie/contation/com/contagion/Login.java
+++ app/src/main/java/tag/zombie/contation/com/contagion/Login.java
@@ -1,13 +1,34 @@
 package tag.zombie.contation.com.contagion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    EditText nameEditText;
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nameEditText = (EditText)findViewById(R.id.nameEditText);
+        playButton = (Button)findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(buttonListener);
     }
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(),GameActivity.class);
+            startActivity(intent);
+        }
+    };
+
+
 }

a0bd87c045c1730e4a4b77845479bef0ff877909
Sultan Khan <khansultan024@gmail.com>
2016-01-22 20:33:22 -0800

    Added all activities

---
+++ app/src/main/java/tag/zombie/contation/com/contagion/GameListActivity.java
@@ -0,0 +1,14 @@
+package tag.zombie.contation.com.contagion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_list);
+
+    }
+}

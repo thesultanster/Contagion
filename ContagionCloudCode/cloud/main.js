5ada1a00900c292a3da2d9b2ff75e222d12f9cc8
mvill025 <thisismichael011@gmail.com>
2016-01-26 16:09:48 -0800

    finished the addPersonToGame Cloud Code and use it in Android

--- ContagionCloudCode/cloud/main.js
+++ ContagionCloudCode/cloud/main.js
@@ -24,23 +24,37 @@
 
 // });
 
-// addPersonToGame
+/* addPersonToGame
+    input:  user    -   user pointer (assumes taken from getCurrentUser()
+            gameID  -   game objectid (passed in as a string)
+    output: success should return a yay of some sort
+*/
 Parse.Cloud.define("addPersonToGame", function(request, response) {
-  
-  var userId = request.params.userId
-  var Game = Parse.Object.extend("Game");
-  var query = new Parse.Query(Game);
-      query.equalTo("isWaiting", true);
-      query.first({
-    success: function(object) {
+
+  //assumes request
+  var user = request.params.user;
+  var gameId  = request.params.gameId;
+  var query = new Parse.Query("Game");
+
+  // query.equalTo("gameState", 0);
+  query.equalTo("objectId", gameId);
+
+  query.first({
+    success: function(post) {
       // Successfully retrieved the object.
-      object.addUnique("players", userId);
-      object.save()
+      post.addUnique("players", user);
+      post.addUnique("healthyPlayers", user);
+      post.increment("healthyCount");
+      post.save();
+      response.success("***Added Player to the Game!***\t " + post.get("objectId"));
     },
     error: function(error) {
       alert("Error: " + error.code + " " + error.message);
+      response.error("***Did not add player to the game*** \t");
     }
   });
+
+  // response.success("***addPersonToGame is DONE***");
 
   // var query = new Parse.Query("Review");
   // query.equalTo("movie", request.params.movie);
@@ -56,3 +70,4 @@ Parse.Cloud.define("addPersonToGame", function(request, response) {
   //     response.error("movie lookup failed");
   //   }
 });
+
